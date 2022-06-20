206cc5a362d4b49c065056f9f8effa4944ece336
dgmodesto <douglasgomesmodesto@gmail.com>
2022-06-20 09:26:30 -0300

    feat(algorithm): to implement the challenge from hacker rank

---
+++ hackerrank/find_percentage.py
@@ -0,0 +1,32 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# INPUT
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# OUTPUT
+# 56.00
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    filtered = []
+    
+    for (k, v) in student_marks.items():
+        if k == query_name:
+            filtered.append(v)
+    filtered = filtered[0]      
+    result = round(sum(filtered) / len(filtered), 2)    
+    if(len(str(result)) < 5):
+        print(str(result) + '0')
+    else:
+        print(result)
